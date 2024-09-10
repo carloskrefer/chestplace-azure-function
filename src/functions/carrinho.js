0080cbfe9f00cb105a80b524460400cca9ac003c
Carlos Krefer <carloskrefer@hotmail.com>
2024-09-10 10:50:29 -0300

    Adiciona conexão de teste no MongoDB

--- src/functions/carrinho.js
+++ src/functions/carrinho.js
@@ -1,14 +1,49 @@
 const { app } = require('@azure/functions');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://carloskrefer:3AFSiTE3n8EY0Q7Q@chestplace.pvyxw.mongodb.net/?retryWrites=true&w=majority&appName=chestplace";
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
 
 app.http('carrinho', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        let erroBanco = false;
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        run().catch((error) => {
+            console.log(error);
+            erroBanco = true;
+        });
 
-        return { body: `Hello, ${name}!` };
+        if (erroBanco) {
+            return { body: `Erro no MongoDB!` };
+        } else {
+            return { body: `Hello, tudo certo no MongoDB!` };
+        }
+
+        // context.log(`Http function processed request for url "${request.url}"`);
+
+        // const name = request.query.get('name') || await request.text() || 'world';
+
+        // return { body: `Hello, ${name}!` };
     }
 });
+
+
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
 
