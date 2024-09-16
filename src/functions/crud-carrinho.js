921593691582877fbda2fddde4183d8422561917
Carlos Krefer <carloskrefer@hotmail.com>
2024-09-15 23:58:29 -0300

    Refatora

--- src/functions/crud-carrinho.js
+++ src/functions/crud-carrinho.js
@@ -1,67 +1,16 @@
+//##############################################//
+//                                              //
+//                   Imports                    //
+//                                              //
+//##############################################//
 const { app } = require('@azure/functions');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-class CarrinhoModel {
-    compradorId;
-    produtos;
-
-    validate() {
-        ValidatorHelper.validateNumberRequired(this.compradorId, 'compradorId');
-        this.#validateProdutos();
-    }
-
-    #validateProdutos() {
-        ValidatorHelper.validateArray(this.produtos, 'produtos');
-
-        if (this.produtos.some(produto => !(produto instanceof ProdutoModel)))
-            throw new ObjectTypeException(
-                'Atribuída a classe Carrinho elementos que não são instância de Produto.'
-            );
-
-        this.produtos.forEach(produto => produto.validate());
-    }
-}
-
-class ProdutoModel {
-    id;
-    titulo;
-    preco;
-    preco_promocional;
-    descricao;
-
-    validate() {
-        ValidatorHelper.validateNumberRequired(this.id, 'id');
-        ValidatorHelper.validateStringRequired(this.titulo, 'titulo');
-        ValidatorHelper.validateNumberRequired(this.preco, 'preco');
-        ValidatorHelper.validateNumberRequired(this.preco_promocional, 'preco_promocional');
-        ValidatorHelper.validateStringRequired(this.descricao, 'descricao');
-    }
-}
-
-class ValidatorHelper {
-    static validateNumberRequired(value, description) {
-        if (!isNumber(value))
-            throw new ParameterTypeException(
-                `Atributo "${description}" é obrigatório e deve ser um número.`
-            );
-    }
-
-    static validateStringRequired(value, description) {
-        if (!value)
-            throw new ParameterNotProvidedException(
-                `Atributo "${description}" é obrigatório e não pode ser nulo, vazio ou indefinido.`
-            );
-    }
-
-    static validateArray(array, description) {
-        if (!Array.isArray(array))
-            throw new ParameterTypeException(
-                `Atributo "${description}" deverá ser array.`
-            );
-    }
-}
-
-/* HTTP status codes */
+//##############################################//
+//                                              //
+//              HTTP Status Codes               //
+//                                              //
+//##############################################//
 const statusCode = {
     created: 201,
     noContent: 204,
@@ -71,14 +20,11 @@ const statusCode = {
     internalServerError: 500
 };
 
-/* Exceptions */
-// test and make them be classes and extend error
-function QueryParamNotProvidedException(message) { //remove if not used
-    this.message = message;
-    this.name = QueryParamNotProvidedException.name;
-    this.statusCode = statusCode.badRequest;
-}
-
+//##############################################//
+//                                              //
+//        Exceptions with status codes          //
+//                                              //
+//##############################################//
 function RecordNotFoundException(message) {
     this.message = message;
     this.name = RecordNotFoundException.name;
@@ -121,13 +67,232 @@ function DuplicateSingletonException(message) {
     this.statusCode = statusCode.internalServerError;
 }
 
+//##############################################//
+//                                              //
+//                    Models                    //
+//                                              //
+//##############################################//
+class CarrinhoModel {
+    compradorId;
+    produtos;
+
+    validate() {
+        ValidatorHelper.validateNumberRequired(this.compradorId, 'compradorId');
+        this.#validateProdutos();
+    }
+
+    #validateProdutos() {
+        ValidatorHelper.validateArray(this.produtos, 'produtos');
+
+        if (this.produtos.some(produto => !(produto instanceof ProdutoModel)))
+            throw new ObjectTypeException(
+                'Atribuída a classe Carrinho elementos que não são instância de Produto.'
+            );
+
+        this.produtos.forEach(produto => produto.validate());
+    }
+}
+
+class ProdutoModel {
+    id;
+    titulo;
+    preco;
+    preco_promocional;
+    descricao;
+
+    validate() {
+        ValidatorHelper.validateNumberRequired(this.id, 'id');
+        ValidatorHelper.validateStringRequired(this.titulo, 'titulo');
+        ValidatorHelper.validateNumberRequired(this.preco, 'preco');
+        ValidatorHelper.validateNumberRequired(this.preco_promocional, 'preco_promocional');
+        ValidatorHelper.validateStringRequired(this.descricao, 'descricao');
+    }
+}
+
+//##############################################//
+//                                              //
+//                   Helpers                    //
+//                                              //
+//##############################################//
+class ValidatorHelper {
+    static validateNumberRequired(value, description) {
+        if (!NumberHelper.isNumber(value))
+            throw new ParameterTypeException(
+                `Atributo "${description}" é obrigatório e deve ser um número.`
+            );
+    }
+
+    static validateStringRequired(value, description) {
+        if (!value)
+            throw new ParameterNotProvidedException(
+                `Atributo "${description}" é obrigatório e não pode ser nulo, vazio ou indefinido.`
+            );
+    }
+
+    static validateArray(array, description) {
+        if (!Array.isArray(array))
+            throw new ParameterTypeException(
+                `Atributo "${description}" deverá ser array.`
+            );
+    }
+}
+
+class ObjectHelper {
+    static isObject(value) {
+        return typeof (value) === 'object';
+    }
+}
+
+class NumberHelper {
+    // Observation: this function will also return FALSE if value is NaN.
+    static isNumber(value) {
+        if (isNaN(value))
+            return false;
+
+        return typeof (value) === 'number';
+    }
+
+    // Checks if the parameter is exactly NaN.
+    static isNaN(value) {
+        if (ObjectHelper.isObject(value))
+            return false;
+
+        return value !== value;
+    }
+}
+
+//##############################################//
+//                                              //
+//                  Services                    //
+//                                              //
+//##############################################//
+class CarrinhoService {
+    static instance;
+    static carrinhoDAO;
+
+    constructor() {
+        if (this.instance)
+            throw new DuplicateSingletonException(
+                'Singleton CarrinhoService already exists.'
+            );
+
+        this.instance = this;
+        this.carrinhoDAO = CarrinhoDAO.getInstance();
+    }
+
+    static getInstance() {
+        if (!this.instance)
+            this.instance = new CarrinhoService();
+
+        return this.instance;
+    }
+
+    async selectCarrinho(compradorId) {
+        const carrinho = await this.carrinhoDAO.getOneCarrinhoByCompradorId(compradorId);
+
+        if (!carrinho)
+            throw new RecordNotFoundException(
+                'Não há carrinho para o "compradorId" informado.'
+            );
+
+        return carrinho;
+    }
+
+    async insertCarrinho(carrinhoModel) {
+        const carrinho =
+            await this.carrinhoDAO.getOneCarrinhoByCompradorId(carrinhoModel.compradorId);
+
+        if (carrinho)
+            throw new RecordAlreadyExistsException(
+                'Carrinho já cadastrado para o usuário informado.'
+            );
+
+        await this.carrinhoDAO.postOneCarrinho(carrinhoModel);
+    }
+
+    async deleteCarrinho(compradorId) {
+        const carrinho = await this.carrinhoDAO.getOneCarrinhoByCompradorId(compradorId);
+
+        if (!carrinho)
+            throw new RecordNotFoundException(
+                'Não há carrinho para o "compradorId" informado.'
+            );
+
+        await this.carrinhoDAO.deleteOneCarrinhoByCompradorId(compradorId);
+    }
+
+    async updateCarrinho(carrinhoModel) {
+        const carrinho =
+            await this.carrinhoDAO.getOneCarrinhoByCompradorId(carrinhoModel.compradorId);
+
+        if (!carrinho)
+            throw new RecordNotFoundException(
+                'Não há carrinho para o "compradorId" informado.'
+            );
+
+        await this.carrinhoDAO.updateOneCarrinho(carrinhoModel);
+    }
+}
+
+class MongoDBService {
+    static URI =
+        "mongodb+srv://carloskrefer:3AFSiTE3n8EY0Q7Q@chestplace.pvyxw.mongodb.net/?retryWrites=true&w=majority&appName=chestplace";
+    static CLIENT = new MongoClient(MongoDBService.URI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    static DATABASE_NAME = "chestplace";
+    static COLLECTION_NAME = "carrinhos";
+    static instance;
+
+    constructor() {
+        if (this.instance)
+            throw new DuplicateSingletonException('Singleton MongoDB already exists.');
+
+        this.instance = this;
+    }
+
+    static getInstance() {
+        if (!this.instance)
+            this.instance = new MongoDBService();
+
+        return this.instance;
+    }
+
+    async executeDatabaseCommand(callback) {
+        try {
+            await MongoDBService.CLIENT.connect();
+            const database = MongoDBService.CLIENT.db(MongoDBService.DATABASE_NAME);
+            const collection = database.collection(MongoDBService.COLLECTION_NAME);
+            return await callback(collection);
+        } catch (exception) {
+            const message =
+                'Ocorreu um erro durante a conexão com o banco de dados: '
+                + exception.message;
+            throw new DatabaseConnectionException(message);
+        } finally {
+            await MongoDBService.CLIENT.close();
+        }
+    }
+}
+
+//##############################################//
+//                                              //
+//                  Controller                  //
+//                                              //
+//##############################################//
 class CarrinhoController {
     static instance;
     static carrinhoService;
 
     constructor() {
         if (this.instance)
-            throw new DuplicateSingletonException('Singleton CarrinhoService already exists.');
+            throw new DuplicateSingletonException(
+                'Singleton CarrinhoService already exists.'
+            );
 
         this.instance = this;
         this.carrinhoService = CarrinhoService.getInstance();
@@ -207,23 +372,8 @@ class CarrinhoController {
         const compradorId = Number(request.params.compradorId);
         ValidatorHelper.validateNumberRequired(compradorId, 'compradorId');
         const { produtos } = JSON.parse(await request.text());
-
-        let produtosInstances = [];
-        produtos.forEach(produto => {
-            const produtoInstance = new ProdutoModel();
-            produtoInstance.id = Number(produto.id);
-            produtoInstance.titulo = produto.titulo;
-            produtoInstance.preco = Number(produto.preco);
-            produtoInstance.preco_promocional = Number(produto.preco_promocional);
-            produtoInstance.descricao = produto.descricao;
-            produtosInstances.push(produtoInstance);
-        });
-
-        const carrinhoModel = new CarrinhoModel();
-        carrinhoModel.compradorId = Number(compradorId);
-        carrinhoModel.produtos = produtosInstances;
+        const carrinhoModel = this.#mapDataToCarrinhoModel(compradorId, produtos);
         carrinhoModel.validate();
-
         await this.carrinhoService.updateCarrinho(carrinhoModel);
 
         return { status: statusCode.noContent };
@@ -239,21 +389,7 @@ class CarrinhoController {
 
     async #getPostResponse(request) {
         const { compradorId, produtos } = JSON.parse(await request.text());
-
-        let produtosInstances = [];
-        produtos.forEach(produto => {
-            const produtoInstance = new ProdutoModel();
-            produtoInstance.id = Number(produto.id);
-            produtoInstance.titulo = produto.titulo;
-            produtoInstance.preco = Number(produto.preco);
-            produtoInstance.preco_promocional = Number(produto.preco_promocional);
-            produtoInstance.descricao = produto.descricao;
-            produtosInstances.push(produtoInstance);
-        });
-
-        const carrinhoModel = new CarrinhoModel();
-        carrinhoModel.compradorId = Number(compradorId);
-        carrinhoModel.produtos = produtosInstances;
+        const carrinhoModel = this.#mapDataToCarrinhoModel(compradorId, produtos);
         carrinhoModel.validate();
         await this.carrinhoService.insertCarrinho(carrinhoModel);
 
@@ -266,79 +402,31 @@ class CarrinhoController {
             body: exception.message
         };
     }
-}
 
-class ObjectHelper {
-    static isObject(value) {
-        return typeof (value) === 'object';
+    #mapDataToCarrinhoModel(compradorId, produtos) {
+        let produtosInstances = [];
+        produtos.forEach(produto => {
+            const produtoInstance = new ProdutoModel();
+            produtoInstance.id = Number(produto.id);
+            produtoInstance.titulo = produto.titulo;
+            produtoInstance.preco = Number(produto.preco);
+            produtoInstance.preco_promocional = Number(produto.preco_promocional);
+            produtoInstance.descricao = produto.descricao;
+            produtosInstances.push(produtoInstance);
+        });
+        const carrinhoModel = new CarrinhoModel();
+        carrinhoModel.compradorId = Number(compradorId);
+        carrinhoModel.produtos = produtosInstances;
+
+        return carrinhoModel;
     }
 }
 
-class CarrinhoService {
-    static instance;
-    static carrinhoDAO;
-
-    constructor() {
-        if (this.instance)
-            throw new DuplicateSingletonException('Singleton CarrinhoService already exists.');
-
-        this.instance = this;
-        this.carrinhoDAO = CarrinhoDAO.getInstance();
-    }
-
-    static getInstance() {
-        if (!this.instance)
-            this.instance = new CarrinhoService();
-
-        return this.instance;
-    }
-
-    async selectCarrinho(compradorId) {
-        const carrinho = await this.carrinhoDAO.getOneCarrinhoByCompradorId(compradorId);
-
-        if (!carrinho)
-            throw new RecordNotFoundException(
-                'Não há carrinho para o "compradorId" informado.'
-            );
-        
-        return carrinho;
-    }
-
-    async insertCarrinho(carrinhoModel) {
-        const carrinho = await this.carrinhoDAO.getOneCarrinhoByCompradorId(carrinhoModel.compradorId);
-
-        if (carrinho)
-            throw new RecordAlreadyExistsException(
-                'Carrinho já cadastrado para o usuário informado.'
-            );
-
-        await this.carrinhoDAO.postOneCarrinho(carrinhoModel);
-    }
-
-    async deleteCarrinho(compradorId) {
-        const carrinho = await this.carrinhoDAO.getOneCarrinhoByCompradorId(compradorId);
-
-        if (!carrinho)
-            throw new RecordNotFoundException(
-                'Não há carrinho para o "compradorId" informado.'
-            );
-
-        await this.carrinhoDAO.deleteOneCarrinhoByCompradorId(compradorId);
-    }
-
-    async updateCarrinho(carrinhoModel) {
-        const carrinho = await this.carrinhoDAO.getOneCarrinhoByCompradorId(carrinhoModel.compradorId);
-
-        if (!carrinho)
-            throw new RecordNotFoundException(
-                'Não há carrinho para o "compradorId" informado.'
-            );
-
-        await this.carrinhoDAO.updateOneCarrinho(carrinhoModel);
-    }
-}
-
-// checar instanceof do argumento
+//##############################################//
+//                                              //
+//                     DAO                      //
+//                                              //
+//##############################################//
 class CarrinhoDAO {
     static instance;
     static mongoDB;
@@ -348,7 +436,7 @@ class CarrinhoDAO {
             throw new DuplicateSingletonException('Singleton CarrinhoDAO already exists.');
 
         this.instance = this;
-        this.mongoDB = MongoDB.getInstance();
+        this.mongoDB = MongoDBService.getInstance();
     }
 
     static getInstance() {
@@ -395,69 +483,9 @@ class CarrinhoDAO {
     }
 }
 
-class MongoDB {
-    static URI = "mongodb+srv://carloskrefer:3AFSiTE3n8EY0Q7Q@chestplace.pvyxw.mongodb.net/?retryWrites=true&w=majority&appName=chestplace";
-    static CLIENT = new MongoClient(MongoDB.URI, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-    static DATABASE_NAME = "chestplace";
-    static COLLECTION_NAME = "carrinhos";
-
-    static instance;
-
-    constructor() {
-        if (this.instance)
-            throw new DuplicateSingletonException('Singleton MongoDB already exists.');
-
-        this.instance = this;
-    }
-
-    static getInstance() {
-        if (!this.instance)
-            this.instance = new MongoDB();
-
-        return this.instance;
-    }
-
-    async executeDatabaseCommand(callback) {
-        try {
-            await MongoDB.CLIENT.connect();
-            const database = MongoDB.CLIENT.db(MongoDB.DATABASE_NAME);
-            const collection = database.collection(MongoDB.COLLECTION_NAME);
-            return await callback(collection);
-        } catch (exception) {
-            const message =
-                'Ocorreu um erro durante a conexão com o banco de dados: '
-                + exception.message;
-            throw new DatabaseConnectionException(message);
-        } finally {
-            await MongoDB.CLIENT.close();
-        }
-    }
-}
-
-// Observation: this functions will return FALSE if value is NaN.
-function isNumber(value) {
-    if (isNaN(value))
-        return false;
-
-    return typeof (value) === 'number';
-}
-
-// Checks if the parameter is NaN.
-function isNaN(value) {
-    if (isObject(value))
-        return false;
-
-    return value !== value;
-}
-
-function isObject(value) {
-    return typeof (value) === 'object';
-}
-
+//##############################################//
+//                                              //
+//             Controller registry              //
+//                                              //
+//##############################################//
 CarrinhoController.getInstance().registerRoutes(app);
